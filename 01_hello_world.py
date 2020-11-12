17e62649340b7702916326e182c7d5a7bef7dc4f
dengjw <dengjianwei@7tao.net>
2020-11-12 15:48:03 +0800

    列表

---
+++ 01_hello_world.py
@@ -0,0 +1,47 @@
+# message = 'Hello World'
+# print(message)
+
+# 打印
+# message = 'Hello World'
+# print(message)
+# message = 'Hello Python'
+# print(message)
+
+# 修改大小写
+# name = 'ada lovelace'
+# print(name.title()) # Ada Lovelace
+# print(name.upper()) # ADA LOVELACE
+# print(name.lower()) # ada lovelace
+
+# 拼接
+# first_name = 'deng'
+# last_name = 'jim'
+# full_name = first_name + ' ' + last_name
+# print('Hello, ' + full_name.title() + '!')
+
+# 制表符
+# print('\tPython')
+# 换行符
+# print('Python \n C')
+
+## 删除多余空格
+# lang = ' Python '
+# print(lang)
+# print(lang.rstrip()) # 删除右边的空格
+# print(lang.lstrip()) # 删除左边的空格
+# print(lang.strip()) # 删除两边的空格
+
+### 数字
+
+# print( 2 + 3 )
+# print( 2 - 3 )
+# print( 2 * 3 )
+# print( 2 / 3 )
+# 乘方
+# print(2 ** 3)
+# 浮点数
+# print(.2 + .1) # 0.30000000000000004
+# 使用str()
+# age = 23
+# print('Happy ' + str(age) + 'rd Birthday!')
+import this # Python 之禅
---
+++ 02_列表.py
@@ -0,0 +1,17 @@
+# 列表
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles)
+# print(bicycles[0])
+# print(bicycles[-1])
+
+# 修改列表元素
+# bicycles[0] = 'honda'
+# print(bicycles)
+
+# 列表末尾添加元素
+# bicycles.append('fenghuang')
+# print(bicycles)
+
+# 在列表的任意位置插入元素
+bicycles.insert(0, 'hafou')
+print(bicycles)
